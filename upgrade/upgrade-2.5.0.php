407b6f7f1172b8b5cb3948c04c9bfa970cb68cf7
Emanuele Coppola <emanuele.coppola99@gmail.com>
2024-09-29 21:32:59 +0200

    Remove unused Satispay state

---
+++ upgrade/upgrade-2.5.0.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Satispay\Prestashop\Upgrade;
+
+// Prestashop
+use \Configuration;
+use \OrderState;
+use \Validate;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_2_5_0($module) {
+    /** @var Satispay $module */
+
+    // remove Satispay order state
+    $orderStateId = (int) Configuration::get('SATISPAY_PENDING_STATE');
+
+    if ($orderStateId) {
+        $orderState = new OrderState($orderStateId);
+
+        if (Validate::isLoadedObject($orderState)) {
+            return $orderState->delete() && Configuration::deleteByName('SATISPAY_PENDING_STATE');
+        }
+    }
+
+    return true;
+}
