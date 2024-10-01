01367d2125ebab41173b32cd3e585f71fdfbd96e
Emanuele Coppola <emanuele.coppola99@gmail.com>
2024-10-02 00:34:52 +0200

    Minor index.php formatting

--- controllers/index.php
+++ controllers/index.php
@@ -5,7 +5,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 header("Expires: Thu, 01 Jul 1999 09:00:00 GMT");
-header("Last-Modified: ". gmdate("D, d M Y H:i:s") . " GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
